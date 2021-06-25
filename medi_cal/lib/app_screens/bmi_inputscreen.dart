fec3ee663c4ca3ac3b88676ed9c789c1f789d1f1
Theodore <30gash@gmail.com>
2021-06-25 06:27:11 +0000

    BMI app functional

--- medi_cal/lib/app_screens/bmi_inputscreen.dart
+++ medi_cal/lib/app_screens/bmi_inputscreen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:medi_cal/app_screens/home_screen.dart';
 
 class BmiInputScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "BMI Calculator",
-        home: Scaffold(
-          appBar: AppBar(title: Text("BMI Calculator"),),
-          body: BmiInputCore (),
-        )
+      debugShowCheckedModeBanner: false,
+      title: "BMI Calculator",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("BMI Calculator"),
+              IconButton(
+                icon: const Icon(Icons.home,
+                    color: Colors.white, size: 24.0, semanticLabel: "Home"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        body: BmiInputCore(),
+      ),
     );
   }
 }
@@ -23,5 +40,173 @@ class BmiInputCore extends StatelessWidget {
         color: Colors.lightBlueAccent,
         child: Center(
             child: Container(
-              padding: EdgeInsets.only(top: 0.0),
-              alignment: Alignment.center,)));}}
+          padding: EdgeInsets.only(top: 0.0),
+          alignment: Alignment.center,
+          child: InputFields(),
+        )));
+  }
+}
+
+// Define a custom Form widget.
+class InputFields extends StatefulWidget {
+  @override
+  _InputFieldsState createState() => _InputFieldsState();
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class _InputFieldsState extends State<InputFields> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  //final _formKey = GlobalKey<FormState>();
+
+  // the controller for the text field associated with "height"
+  final _heightController = TextEditingController();
+
+  // the controller for the text field associated with "weight"
+  final _weightController = TextEditingController();
+
+  double _bmi;
+
+  // the message at the beginning
+  String _message = 'Please enter height and weight';
+
+  void _calculate() {
+    final double height = double.tryParse(_heightController.value.text);
+    final double weight = double.tryParse(_weightController.value.text);
+
+    if (height <= 0 || weight <= 0) {
+      setState(() {
+        _message = "Height and weight must be positive numbers";
+      });
+      return;
+    }
+
+    setState(() {
+      _bmi = weight / (height * height);
+      if (_bmi < 18.5) {
+        _message = "Underweight";
+      } else if (_bmi < 25) {
+        _message = 'Normal BMI';
+      } else if (_bmi < 30) {
+        _message = 'Overweight';
+      } else if (_bmi < 40) {
+        _message = 'Obese';
+      } else {
+        _message = 'Morbidly Obese';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        body: Center(
+          child: Container(
+            width: 320,
+            child: Card(
+              color: Colors.white,
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(labelText: 'Height (m)'),
+                      controller: _heightController,
+                    ),
+                    TextField(
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        labelText: 'Weight (kg)',
+                      ),
+                      controller: _weightController,
+                    ),
+                    ElevatedButton(
+                      onPressed: _calculate,
+                      child: Text('Calculate'),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      child: Text(
+                        _bmi == null ? 'No Result' : _bmi.toStringAsFixed(2),
+                        style: TextStyle(fontSize: 50),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Text(
+                        _message,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+/*@override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child:
+      Column(
+          children: [
+            TextField(
+              decoration: new InputDecoration(labelText: "Enter age (in years)"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],),
+            TextField(
+              decoration: new InputDecoration(labelText: "Enter weight (in kg)"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],),
+            TextField(
+              decoration: new InputDecoration(labelText: "Enter height (in m)"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],),
+            Container(
+            margin: EdgeInsets.only(top: 70.0),
+            width: 250.0,
+            height: 50.0,
+            child:
+              ElevatedButton(
+              onPressed: () {},
+                child: Text('Calculate BMI',
+                    style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    ),),
+          ),
+          // Add TextFormFields and ElevatedButton here.
+
+    ),
+      ],
+    ));
+  }*/
+
+//class CalculateBMI
--- medi_cal/lib/app_screens/welcome_screen.dart
+++ medi_cal/lib/app_screens/welcome_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 //import 'package:medi_cal/back_app_screens/back_home_screen.dart';
-import 'package:medi_cal/back_app_screens/back_welcome_screen.dart';
-
 import 'home_screen.dart';
 //import './app_screens/home_screen.dart';
 //import 'home_screen.dart';
